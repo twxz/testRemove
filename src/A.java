0a806481382de9c513928a9095540acda0642745
twxz <twxz_github@163.com>
2022-07-08 11:07:18 +0800

    7-8-1102

--- src/A.java
+++ src/A.java
@@ -6,10 +6,12 @@ public class A {
             System.out.println("this is 3");
             System.out.println("this is a");
             System.out.println("this is 4");
-            System.out.println("this is 511");
-            System.out.println("this is 6");
+            System.out.println("this is 5");
+            System.out.println("this is 666");
+            System.out.println("this is b");
         }else{
             System.out.println("this is 20");
+            System.out.println("this is 21");
         }
     }
 }
