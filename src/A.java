2c2541fb91cddd97756ff010110deee085c2966b
twxz <108866073+twxz@users.noreply.github.com>
2022-07-08 10:25:11 +0800

    Update A.java
    20220708-增加else

--- src/A.java
+++ src/A.java
@@ -1,11 +1,14 @@
 public class A {
     public static void main(String[] args) {
         if (true) {
-            System.out.println("this is 1");
+            System.out.println("this is 111");
             System.out.println("this is 2");
             System.out.println("this is 3");
             System.out.println("this is 4");
-            System.out.println("this is 5");
+            System.out.println("this is 511");
+            System.out.println("this is 6");
+        }else{
+            System.out.println("this is 20");
         }
     }
 }
